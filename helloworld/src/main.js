ef29fcc86988b80eba3b169e66d51db101160acd
Ronei Macedo Gualberto <ronei@prodam.am.gov.br>
2018-06-08 09:10:04 -0400

    Voltando ao inicio do helloworld

--- helloworld/src/main.js
+++ helloworld/src/main.js
@@ -3,12 +3,5 @@ import App from './App.vue'
 
 new Vue({
   el: '#app',
-  render: (createElement) => {
-      return createElement('header',
-               {attrs: {id: 'elemento'}},
-              [
-                createElement('h1','Título do post'),
-                createElement('p','Conteudo do post')
-            ]);
-  }
+  render: h => h(App)
 })
