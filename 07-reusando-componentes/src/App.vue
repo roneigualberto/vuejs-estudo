7a8698a50a3e52636b17e0b6656e4dedcdd444db
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2018-06-10 17:08:38 -0400

    Reusando componentes com props

---
+++ 07-reusando-componentes/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <h4>Lista um</h4>
+
+    <lv-lista :lista="item in ['feijão','arroz', 'carne']"></lv-lista>
+
+    <lv-lista :lista="item in ['arroz','carne', 'feijão']"></lv-lista>
+
+    <br>
+
+    <h4>Lista dois</h4>
+
+    <lv-lista :lista="['leite','biscoito', 'bombom','aveia']"></lv-lista>
+
+    <lv-lista :lista="['aveia','biscoito', 'bombom', 'leite']"></lv-lista>
+
+
+
+  </div>
+</template>
+
+<script>
+import LvLista from "./LvLista.vue";
+
+export default {
+  name: "app",
+  components: { LvLista }
+};
+</script>
+
+<style>
+li {
+  display: inline;
+  list-style-type: none;
+  padding-right: 20px;
+  float: left;
+}
+</style>
---
+++ 07-reusando-componentes/src/LvLista.vue
@@ -0,0 +1,30 @@
+<template>
+ <div>
+  asdfasd
+ </div>
+
+</template>
+
+<script>
+export default {
+  name: "LvLista",
+  props: {
+    ordena: null,
+    lista: {
+      required: true,
+      type: Array,
+      default: []
+    }
+  },
+  created() {
+    if (typeof this.ordena != "undefined") {
+      this.lista.sort();
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
+
